a5aff2aea60a591e2fc01491d880fdbc0f729479
vamsi <vamsi@Subbu_Puvvada>
2022-10-25 21:39:29 +0530

    Second commit

--- Java_Selenium_Project/src/Basic.java
+++ Java_Selenium_Project/src/Basic.java
@@ -14,6 +14,7 @@ public class Basic {
 		System.out.println("789456");
 		
 		
+		
 	}
 
 }
---
+++ Java_Selenium_Project/src/Basic2.java
@@ -0,0 +1,12 @@
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		System.out.println("Its working man");
+		
+		
+	}
+
+}
